251cc9b3c4ccc48eef601fa3d26f2bf6aaaf8b3c
mba3gar <ramiatrouni@gmail.com>
2019-08-19 12:51:33 +0300

    Changes

--- index.js
+++ index.js
@@ -16,4 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', require('./routes/index'))
 app.use('/api/url', require('./routes/url'))
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log("App Working on the following LINK", process.env.baseUrl);
+});
